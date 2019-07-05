32a8b1f8ff26aefeea5d8234974117b7c2ea6ba4
rami__50 <ramibnkseel@gmail.com>
2019-07-05 22:30:29 +0530

    Updates

---
+++ precache-manifest.6f5c50b9cf67955aed949618a4d449e4.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "91191f04ac27f3aae19c1d429860dff9",
+    "url": "/fruit/static/media/apricot.91191f04.jpg"
+  },
+  {
+    "revision": "86bc4fa76fa43ee189a1",
+    "url": "/fruit/static/css/main.01956ae2.chunk.css"
+  },
+  {
+    "revision": "1f1e656370ec72c82370",
+    "url": "/fruit/static/js/runtime~main.a33fa0b3.js"
+  },
+  {
+    "revision": "5bb1ef3de69e73c248b13d48a9b16e0e",
+    "url": "/fruit/static/media/watermelon.5bb1ef3d.jpg"
+  },
+  {
+    "revision": "c2314e95983044d57074",
+    "url": "/fruit/static/js/2.b9c9e56b.chunk.js"
+  },
+  {
+    "revision": "51c9155191ec193abb3925ebe8e6e646",
+    "url": "/fruit/static/media/farm.51c91551.jpg"
+  },
+  {
+    "revision": "cff186d420ab69731317a355b6588adc",
+    "url": "/fruit/static/media/1.cff186d4.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fruit/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fruit/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fruit/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fruit/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fruit/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af5caf878f540b3029ccc4f39bc31225",
+    "url": "/fruit/static/media/fre.af5caf87.jpg"
+  },
+  {
+    "revision": "cb8b3e78b8bc62508bed0e6c574c7fe0",
+    "url": "/fruit/static/media/del.cb8b3e78.jpg"
+  },
+  {
+    "revision": "f7b9a67e6f6f6c0d3918d2c367eed334",
+    "url": "/fruit/static/media/healthy.f7b9a67e.png"
+  },
+  {
+    "revision": "6e160fce51c46df63c411ff2c6341461",
+    "url": "/fruit/static/media/Pomegranate.6e160fce.jpg"
+  },
+  {
+    "revision": "542811c56713c7cdfa59a7c58ad471d0",
+    "url": "/fruit/static/media/apple.542811c5.jpg"
+  },
+  {
+    "revision": "86bc4fa76fa43ee189a1",
+    "url": "/fruit/static/js/main.ee3c1df9.chunk.js"
+  },
+  {
+    "revision": "d5f49b9a5910186f3d57c18f4f8a0186",
+    "url": "/fruit/static/media/banana.d5f49b9a.jpg"
+  },
+  {
+    "revision": "784e6ee5d6bdeff59a5610864b04e0f1",
+    "url": "/fruit/static/media/blueberry.784e6ee5.jpg"
+  },
+  {
+    "revision": "8c7e3be9e4952ab5729f26f7b2632584",
+    "url": "/fruit/static/media/cherry.8c7e3be9.jpg"
+  },
+  {
+    "revision": "1c551584c6ecca0ec6a83293a1f202c2",
+    "url": "/fruit/static/media/fresh.1c551584.jpg"
+  },
+  {
+    "revision": "260148a76d9ed2dac60407e92573bd85",
+    "url": "/fruit/static/media/grape.260148a7.jpg"
+  },
+  {
+    "revision": "47d26ac922a166924131375697a1b168",
+    "url": "/fruit/static/media/guayaba.47d26ac9.jpg"
+  },
+  {
+    "revision": "1c3e082c7c4c33195aff6a37fc4b508c",
+    "url": "/fruit/static/media/kiwi.1c3e082c.jpg"
+  },
+  {
+    "revision": "9099e4b285676687158245c45e5b4779",
+    "url": "/fruit/static/media/mango.9099e4b2.jpg"
+  },
+  {
+    "revision": "448b530c63de42b7cb7fd811f5c536bb",
+    "url": "/fruit/static/media/melon.448b530c.jpg"
+  },
+  {
+    "revision": "75d5612e33b489f8ea1e7212c7309290",
+    "url": "/fruit/static/media/orange.75d5612e.jpg"
+  },
+  {
+    "revision": "872e11769e4f452e8c6ad08f0b367ff8",
+    "url": "/fruit/static/media/peach.872e1176.jpg"
+  },
+  {
+    "revision": "273c252d620a5c8ebd744decc8de0492",
+    "url": "/fruit/static/media/pear.273c252d.jpg"
+  },
+  {
+    "revision": "8db76cf84eb34c5042c009b60c73449e",
+    "url": "/fruit/static/media/pineaple.8db76cf8.jpg"
+  },
+  {
+    "revision": "5a4c84367f9fa8f27d5236016ceaa496",
+    "url": "/fruit/static/media/plum.5a4c8436.jpg"
+  },
+  {
+    "revision": "9d3b6dd21041e45411d76f360cd3e033",
+    "url": "/fruit/static/media/strewberry.9d3b6dd2.jpg"
+  },
+  {
+    "revision": "c2314e95983044d57074",
+    "url": "/fruit/static/css/2.ed14c851.chunk.css"
+  },
+  {
+    "revision": "59a05ad4690502f26c3df352bc3ac853",
+    "url": "/fruit/index.html"
+  }
+];
